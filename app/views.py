e41827c8c9463789b873b78d55d56631b87faf4d
KauanCentral <kauants@centranet.com.br>
2026-05-12 14:31:54 -0400

    formulario, e metodos get e post

--- app/views.py
+++ app/views.py
@@ -1,11 +1,18 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 
 @app.route('/')
 def homepage():
     usuario = 'Kauan Turcato Storto'
     return render_template('index.html', usuario=usuario)
 
-@app.route('/nova/')
-def novapage():
-    return 'Nova Pagina'
+@app.route('/contato/', methods=['GET', 'POST'])
+def contatopage():
+        if request.method == 'GET':
+            pesquisa = request.args.get('pesquisa')
+
+        if request.method == 'POST':
+            pesquisa = request.form['pesquisa']
+              
+              
+        return render_template('contato.html', pesquisa=pesquisa)
